93ae5d4417409600785849ef52e29b2259ed7bd2
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-06-13 15:38:54 +0200

    remove split_colours option from allbyall_correlations

--- frevede_allbyall_correlations.m
+++ frevede_allbyall_correlations.m
@@ -1,4 +1,4 @@
-function frevede_allbyall_correlations(datamat, varlabels, split_colours)
+function frevede_allbyall_correlations(datamat, varlabels)
 
 % to test
 % datamat = rand(25, 5); varlabels = {'var1','var2','var3','var4','var5'};
@@ -8,15 +8,6 @@ npp = size(datamat,1);
 nvar = size(datamat,2);
 rmatrix = zeros(nvar);
 
-% set colourlist
-if split_colours
-    sc = zeros(length(datamat),3);
-    sc(1:16,1) = 1;
-    sc(17:41,3) = 1;
-else
-    sc = 'k';
-end
-
 % draw fig
 figure;
 count = 0;
@@ -25,18 +16,13 @@ for xvar = 2:nvar
 count = count+1;
 if xvar > yvar % only plot off diagonal
     subplot(nvar-1, nvar-1, count); hold on; axis square;
-    scatter(datamat(:,xvar), datamat(:,yvar), [], sc); lsline; 
+    scatter(datamat(:,xvar), datamat(:,yvar), [], 'k'); lsline; 
     xlabel(varlabels{xvar}, 'Interpreter', 'none'); ylabel(varlabels{yvar}, 'Interpreter', 'none'); 
-    [r,p] = corr(datamat(:,xvar), datamat(:,yvar));
+    [r,p] = corr(datamat(:,xvar), datamat(:,yvar), 'type', 'Spearman');
     t = title(['r = ' num2str(round(r*100)/100), ', p = ', num2str(round(p*100)/100)]);
     rmatrix(xvar,yvar) = r; % also save r values for below summary plot.
-    if split_colours
-        if p < 0.1 t.Color = 'c'; end % highlight in cyan if one-sided significant (uncorrected)
-        if p < 0.05 t.Color = 'm'; end % highlight in magenta if two-sided significant (uncorrected)    
-    else
-        if p < 0.1 scatter(datamat(:,xvar), datamat(:,yvar), 'c'); end % highlight in cyan if one-sided significant (uncorrected)
-        if p < 0.05 scatter(datamat(:,xvar), datamat(:,yvar), 'm'); end  % highlight in magenta if two-sided significant (uncorrected)    
-    end
+    if p < 0.1 scatter(datamat(:,xvar), datamat(:,yvar), 'c'); end % highlight in cyan if one-sided significant (uncorrected)
+    if p < 0.05 scatter(datamat(:,xvar), datamat(:,yvar), 'm'); end  % highlight in magenta if two-sided significant (uncorrected)    
     plot(xlim, [0,0], ':k');  plot([0,0], ylim, ':k'); 
 end
 end
